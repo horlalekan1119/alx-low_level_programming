ec29d98b7dcd3aa5c3398baf506bd4def4296214
horlalekan1119 <fuhadisiaka56@gmail.com>
2023-08-13 05:34:30 -0700

    second alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
